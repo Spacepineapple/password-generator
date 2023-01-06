2c0ef788adb7d4378a6eb4d87c646def91c755d4
Spacepineapple <neal.t.reeves@gmail.com>
2023-01-06 20:07:10 +0000

    Check if all options selected are false, alert user and request again

--- script.js
+++ script.js
@@ -98,7 +98,13 @@ var specialCharacters = [
     let lowerLetter = window.confirm("Should your password include lower case letters? Select ok for yes or cancel for no.");
     let numeric = window.confirm("Should your password include numbers? Select ok for yes or cancel for no.");
     let special = window.confirm("Should your password include special characters? Select ok for yes and cancel for no.");
-    return [passwordLength, upperLetter, lowerLetter, numeric, special];
+    //If no options are selected, alert the user and then ask for prompts again
+    if (!upperLetter && !lowerLetter && !numeric && !special) {
+        window.alert("Cannot generate a password without at least one character type. \n Please choose at least one from lower case, upper case, numeric or special characters.");
+        return getPasswordOptions();
+    } else {
+        return [passwordLength, upperLetter, lowerLetter, numeric, special];
+    } 
   }
   
   // Function for getting a random element from an array
@@ -113,29 +119,37 @@ var specialCharacters = [
     let sourceArray = []
     let passwordArray = [];
     let passwordLength = options[0];
-    let characterCounts = [];
+    //If user has selected uppercase characters, add those to source array
     if (options[1] === true) {
         sourceArray = sourceArray.concat(upperCasedCharacters);
+        //Ensure at least one uppercase character is added to the password
         passwordArray.push(getRandom(upperCasedCharacters));
         passwordLength--;
     } 
+    //If user has selected lowercase characters, add those to source array
     if (options[2] === true) {
         sourceArray = sourceArray.concat(lowerCasedCharacters);
+        //Ensure at least one lowercase character is added to the password
         passwordArray.push(getRandom(lowerCasedCharacters));
         passwordLength--;
     }
+    //If user has selected numeric characters, add those to source array
     if (options[3] === true) {
         sourceArray = sourceArray.concat(numericCharacters);
+        //Ensure at least one numeric character is added to the password
         passwordArray.push(getRandom(numericCharacters));
         passwordLength--;
     }
+    //If user has selected special characters, add those to source array
     if (options[4] === true) {
         sourceArray = sourceArray.concat(specialCharacters);
+        //Ensure at least one special character is added to the password
         passwordArray.push(getRandom(specialCharacters));
         passwordLength--;
     }
 
     while (passwordLength>0) {
+        //Select random characters to fill rest of password
         passwordArray.push(getRandom(sourceArray));
         passwordLength--;
     }
@@ -146,8 +160,11 @@ var specialCharacters = [
     let randomString = "";
     let stringLength = arr.length;
     while (stringLength>0) {
+        //Select a random character from source array
         let randomIndex = Math.floor(Math.random()*stringLength);
+        //Add to randomised password
         randomString+=arr[randomIndex];
+        //Remove that character from the source array
         arr.splice(randomIndex, 1);
         stringLength--;
     }
