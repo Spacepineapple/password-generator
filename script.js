ae8bbb88b73d601d4bcaa6d7973d805d4cd5aa57
Spacepineapple <neal.t.reeves@gmail.com>
2023-01-06 19:23:06 +0000

    Check for selected options and append relevant arrays to larger source array

--- script.js
+++ script.js
@@ -92,25 +92,47 @@ var specialCharacters = [
   function getPasswordOptions() {
     let passwordLength = Number(prompt("How many characters should your password contain? Please enter a number between 10 and 64."));
     while (passwordLength<10||passwordLength>64) {
-        passwordLength = prompt("Sorry, your password must contain between 10 and 64 characters /n How many characters should your password contain? Please enter a number between 10 and 64.")
+        passwordLength = prompt("Sorry, your password must contain between 10 and 64 characters \n How many characters should your password contain? Please enter a number between 10 and 64.")
     }
     let upperLetter = window.confirm("Should your password include upper case letters? Select ok for yes or cacncel for no.");
     let lowerLetter = window.confirm("Should your password include lower case letters? Select ok for yes or cancel for no.");
     let numeric = window.confirm("Should your password include numbers? Select ok for yes or cancel for no.");
     let special = window.confirm("Should your password include special characters? Select ok for yes and cancel for no.");
-
+    return [passwordLength, upperLetter, lowerLetter, numeric, special];
   }
   
   // Function for getting a random element from an array
   function getRandom(arr) {
-  
+    let i = Math.floor(Math.random()*arr.length);
+    return arr[i];
   }
   
   // Function to generate password with user input
   function generatePassword() {
-  
-  }
-  
+    let options = getPasswordOptions();
+    let sourceArray = []
+    let passwordArray = [];
+    let passwordLength = options[0];
+    let characterCounts = [];
+    if (options[1] === true) {
+        sourceArray = sourceArray.concat(upperCasedCharacters);
+        passwordArray.push(getRandom(upperCasedCharacters));
+        passwordLength--;
+    } 
+    if (options[2] === true) {
+        sourceArray = sourceArray.concat(lowerCasedCharacters);
+        passwordArray.push(getRandom(lowerCasedCharacters));
+        passwordLength--;
+    }
+    if (options[3] === true) {
+        sourceArray = sourceArray.concat(numericCharacters);
+        passwordArray.push(getRandom(numericCharacters));
+    }
+    if (options[4] === true) {
+        sourceArray = sourceArray.concat(specialCharacters);
+        passwordArray.push(getRandom(specialCharacters));
+    }
+  }  
   // Get references to the #generate element
   var generateBtn = document.querySelector('#generate');
   
